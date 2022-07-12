a11fa18af25d116f3e256ccf5abd561d91fa844e
DrPRun <sheighd@gmail.com>
2022-07-12 13:35:05 +0300

    Zadacha 6, chetnye nechetnye

---
+++ Zadacha006/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N%2 == 0){
+    Console.WriteLine("Да.");
+}
+else {
+    Console.WriteLine("Нет.");
+}
