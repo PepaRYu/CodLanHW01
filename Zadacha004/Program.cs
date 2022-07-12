c9b59e218d7efe611fe0b1a85ccc3a1f2b3e2e1d
DrPRun <sheighd@gmail.com>
2022-07-12 13:09:06 +0300

    zadacha 4 done

---
+++ Zadacha004/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите три числа");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int therdNumber = Convert.ToInt32(Console.ReadLine());
+
+int maxNumber = firstNumber;
+if (maxNumber < secondNumber){
+    maxNumber = secondNumber;
+}
+if (maxNumber < therdNumber){
+    maxNumber = therdNumber;
+}
+Console.WriteLine("Максимальное число: " + maxNumber);
