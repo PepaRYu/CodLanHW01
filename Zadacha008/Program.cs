32261ad86d42e6d48a0c09b7217f242737f92002
DrPRun <sheighd@gmail.com>
2022-07-12 13:45:58 +0300

    zadacha 008 done

---
+++ Zadacha008/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+Console.WriteLine("Список чётных чисел меньших " + N);
+while (start <= N){
+    if (start %2 == 0){
+        Console.WriteLine(start);
+    }
+    start++;
+}
